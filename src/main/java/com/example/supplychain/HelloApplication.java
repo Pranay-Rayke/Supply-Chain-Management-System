2c84a2312d01ca205e8301d217fb5eb8f02298e1
Pranay Rayke <pranayrayke@gmail.com>
2022-12-18 13:38:13 +0530

    login page

--- src/main/java/com/example/supplychain/HelloApplication.java
+++ src/main/java/com/example/supplychain/HelloApplication.java
@@ -3,6 +3,10 @@ package com.example.supplychain;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -10,8 +14,27 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
 
+    private GridPane loginPage(){
+
+        Label emailLabel = new Label("Email");
+        Label passwordLable = new Label("Password");
+
+        TextField emailTextField = new TextField();
+        PasswordField passwordField = new PasswordField();
+
+        GridPane gridPane = new GridPane();
+        gridPane.add(emailLabel,0,0);
+        gridPane.add(emailTextField,1,0);
+        gridPane.add(passwordLable,0,1);
+        gridPane.add(passwordField,1,1);
+
+        return gridPane;
+    }
+
     private Pane createContent(){
         Pane root = new Pane();
+
+        root.getChildren().addAll(loginPage());
         return root;
     }
     @Override
